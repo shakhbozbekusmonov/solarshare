4dc49a418dc9c935499e6e5d86ad4e1889dabbe2
shakhbozbekusmonov <shakhbozbek.usmonov@gmail.com>
2026-03-10 21:35:09 +0500

    feat(app): add SessionProvider, Toaster, update root layout and prisma client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { Toaster } from '@/components/ui/sonner'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -24,11 +26,14 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='uz'>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<Providers>
+					{children}
+					<Toaster richColors position='top-right' />
+				</Providers>
 			</body>
 		</html>
 	)
